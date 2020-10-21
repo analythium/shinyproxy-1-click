e2bc14f1cdbb4ae444c2bca3a73dbc6a4adc17f7
Peter Solymos <psolymos@gmail.com>
2020-10-20 19:17:03 -0600

    Updated registry login

--- digitalocean/setup.sh
+++ digitalocean/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-# usage: `bash setup.sh -i ~/.ssh/id_rsa -s root@ip_address -f application.yml`
+# Usage 
+#
+# `bash setup.sh -i ~/.ssh/id_rsa -s root@ip_address -f application.yml`
+#
 # -i: ssh key
 # -s: user@ip_address
 # -f: /path/to/application.file
 
-# uncomment lines as needed for registry login
+# Registry login
+#
+# Uncomment lines as needed for registry login.
+# Log in to droplet via ssh and add access token into a file:
+# `echo your_token > ./gitlab_token.txt`
+# this will be used to pass token via stdin. 
+# Change `--username username` to your username.
+
 
 while getopts i:s:f: flag
 do
@@ -20,13 +30,13 @@ echo ">>> Copying $file to droplet"
 scp -i $key $file $server:/etc/shinyproxy/application.yml
 
 ssh -i $key $server /bin/bash << EOF
-#  echo ">>> Logging into registry"
-#  docker login registry.gitlab.com
-  echo ">>> Updating docker images according to application.yaml"
-  wget -O ./update.sh https://raw.githubusercontent.com/analythium/shinyproxy-1-click/master/digitalocean/update.sh
-  bash ./update.sh /etc/shinyproxy/application.yml
-  echo ">>> Restarting ShinyProxy"
-  sudo service shinyproxy restart
-  rm ./update.sh
-  echo ">>> Done"
+#echo ">>> Logging into registry"
+#cat ./gitlab_token.txt | docker login --username username --password-stdin registry.gitlab.com
+echo ">>> Updating docker images according to application.yaml"
+wget -O ./update.sh https://raw.githubusercontent.com/analythium/shinyproxy-1-click/master/digitalocean/update.sh
+bash ./update.sh /etc/shinyproxy/application.yml
+echo ">>> Restarting ShinyProxy"
+sudo service shinyproxy restart
+rm ./update.sh
+echo ">>> Done"
 EOF
