06d6873ef59402acb7e9873a49c54590ee4b4594
Peter Solymos <psolymos@gmail.com>
2021-07-24 21:42:46 -0600

    Packer updates
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

---
+++ digitalocean/packer/scripts/01-setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+## Enable Docker
+systemctl daemon-reload
+systemctl restart docker
+systemctl enable docker
+
+## Pull Docker images
+docker pull registry.gitlab.com/analythium/shinyproxy-hello/hello:latest
+docker pull analythium/shinyproxy-demo:latest
+
+## Onstall ShinyProxy
+export VERSION="2.5.0"
+wget https://www.shinyproxy.io/downloads/shinyproxy_${VERSION}_amd64.deb
+apt install ./shinyproxy_${VERSION}_amd64.deb
+rm shinyproxy_${VERSION}_amd64.deb
+
+## Restart ShinyProxy
+service shinyproxy restart
+
+## Restart Nginx
+service nginx restart
+
+# Setting firewall rules
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow http
+#ufw allow https
+ufw --force enable
+
+echo ===============================
+echo Version info:
+echo 
+echo -------------------------------
+echo ShinyProxy $VERSION
+echo -------------------------------
+java -version 
+echo -------------------------------
+docker -v
+echo -------------------------------
+docker-compose -v
+echo -------------------------------
+nginx -v
+echo -------------------------------
+ufw version
+echo ===============================
