5303c6df16e9d7196531945b5877ab9ae12ac759
Peter Solymos <psolymos@gmail.com>
2024-06-25 13:54:01 -0600

    Install docker in script 01
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- digitalocean/packer/scripts/01-install.sh
+++ digitalocean/packer/scripts/01-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt -qqy update
+sudo apt -qqy install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
 ## Install ShinyProxy
 export VERSION="3.1.1"
 sudo wget -nv https://www.shinyproxy.io/downloads/shinyproxy_${VERSION}_amd64.deb
